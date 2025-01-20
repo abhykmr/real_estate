b9683627f345f77cbf0e24a37f062a8d8ef70d42
ravi1475 <rakshitruhil270@gmail.com>
2025-01-20 14:28:59 +0530

    loginpage

--- server/server.js
+++ server/server.js
@@ -188,6 +188,29 @@ app.post("/api/signup", async (req, res) => {
   }
 });
 
+
+// Login Route
+app.post("/api/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "User not found." });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid credentials." });
+    }
+
+    res.status(200).json({ message: "Login successful!" });
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
+
 // Catch-all route to serve a default response
 app.get("/", (req, res) => {
   res.json({ message: "Hello from Express!" });
