f5050ff61c4529747274d523163dea92512837ca
Abhay <sharma727485@gmail.com>
2025-01-20 12:57:17 +0530

    server improvements

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 
+const MONGO_URI = process.env.MONGO_URI;
+
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(MONGO_URI);
--- server/server.js
+++ server/server.js
@@ -1,19 +1,25 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
+require("dotenv").config();
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const connectDB = require("./config/db.js");
 // const bodyParser = require('body-parser');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 // Middleware to parse JSON requests
 app.use(express.json());
+// dotenv.config();
+app.use(cookieParser());
+app.use(cors());
 
 // Serve static files from the React app
 // app.use(express.static(path.join(__dirname, '../client/build')));
 
-
-app.get('/', (req, res) => {
-    res.json({ message: 'Hello from Express!' });
+app.get("/", (req, res) => {
+  res.json({ message: "Hello from Express!" });
 });
 
 // Catch-all route to serve the React app
@@ -22,6 +28,6 @@ app.get('/', (req, res) => {
 // });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
+  connectDB();
 });
-
