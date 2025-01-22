1f9c6509455ce8e6cc4d4e66d5f420abec3654b8
Abhay <sharma727485@gmail.com>
2025-01-22 15:50:38 +0530

    sfdjdjf

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -11,7 +11,7 @@ const Login = ({ setIsLogin }) => {
     e.preventDefault();
 
     try {
-      const response = await fetch("https://real-estate-backend-u0um.onrender.com", {
+      const response = await fetch("http://localhost:5000/api/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
