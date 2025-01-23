49aa88ad39152a5cce6d86500398a3e86b346baa
AnshikaJaiswal-Hub <anshika5873art2001@gmail.com>
2025-01-23 11:27:03 +0530

    Sales

--- sales/src/App.jsx
+++ sales/src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { BroweserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Home from "../pages/Home";
 
 const App = () => {
